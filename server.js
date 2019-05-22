a820f1a0e744704f1fdc332d31482fec2876ee0e
aditilahare <aditilahare525@gmail.com>
2019-05-22 11:12:23 +0530

    Removed logs.

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log("Port ", process.env.PORT);
-
 app.use(express.static('out'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
